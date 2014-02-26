f0c56f5096216d313c4f1374e5a445cf2b7a1cfd
lamito <alagadniibabidy@yahoo.com>
2014-02-26 00:07:16 -0800

    Browser
    initial Commit

---
+++ FrameBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FrameBrowser
+//
+//  Created by John on 2/25/14.
+//  Copyright (c) 2014 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
